327dcd8ccc67bc6dfd4a5af97e83f6dcef3cd9de
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2022-10-31 20:53:33 -0300

    model usuarioEmpresa

---
+++ Backend/ProjAplicado/src/ProjeAplicado.Business/Models/UsuarioEmpresa.cs
@@ -0,0 +1,17 @@
+﻿using ProjAplicado.Business.Models.Enums;
+
+namespace ProjAplicado.Business.Models
+{
+    public class UsuarioEmpresa : Entity
+    {
+        public string Nome { get; set; }
+
+        public string CNPJ { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public TipoUsuario TipoUsuario { get; set; }
+    }
+}
