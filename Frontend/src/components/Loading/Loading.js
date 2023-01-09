d25f3c4f12d45495ba11faac118f4d0eb0286cc0
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2023-01-09 15:41:41 -0300

    Remove Frontend of Repository

--- Frontend/src/components/Loading/Loading.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-export default function LoadingSpinner() {
-  return (
-    <div className="spinner-container">
-      <div className="loading-spinner"></div>
-    </div>
-  );
-}
--- Frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const Api = axios.create({
-  baseURL: "https://127.0.0.1:7018/api",
-});
-
-export default Api;
